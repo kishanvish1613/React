2ae381c83d0eecb38cd9897c1d0498174fccb7b1
kishanvish1613 <100762979+kishanvish1613@users.noreply.github.com>
2024-12-30 15:14:34 +0530

    20.9 useState question pra

--- 20.9/src/App.jsx
+++ 20.9/src/App.jsx
@@ -14,19 +14,64 @@ function App() {
   // );
 
   // 6.
-  const [value, setValue] = useState([1, 2, 3, 4, 5]);
+  // const [value, setValue] = useState([1, 2, 3, 4, 5]);
 
-  return (
-    <div className="p-4">
-      {/* {value.map((val) => {
-        return <p>{val}</p>;
-      })} */}
+  // return (
+  //   <div className="p-4">
+  //     {/* {value.map((val) => {
+  //       return <p>{val}</p>;
+  //     })} */}
 
-      {value.map( val => <p>{val}</p>)}
+  //     {value.map( val => <p>{val}</p>)}
 
-      <button onClick={() => setValue(() => value.filter((val, index) => index != value.length - 1 ))}>click</button>
-    </div>
-  );
+  //     <button onClick={() => setValue(() => value.filter((val, index) => index != value.length - 1 ))}>click</button>
+  //   </div>
+  // );
+
+	// 7. remove index no=2 element
+	//   const [value, setValue] = useState([1, 2, 3, 4, 5]);
+
+	//   return (
+	//     <div className="p-4">
+	// 		{value.map((val, index) => <p>{val}</p>)}
+	// 		<button onClick={() => setValue(() => value.filter((val, index) => index != 2))}>click</button>
+	//     </div>
+	//   );
+
+	// 8. remove the 'element/2==0'
+	// const [value, setValue] = useState([1, 2, 3, 4, 5, 6, 7, 8]);
+
+	// return (
+	// 	<div className="p-4">
+	// 		{value.map((val, index) => <p>{val}</p>)}
+	// 		<button className="px-6 py-2 bg-blue-500 rounded-md" onClick={() => setValue(() => value.filter((val, index) => val % 2 != 0))}>click</button>
+	// 	</div>
+	// );
+
+	// 9. add a number in last index
+	// const [value, setValue] = useState([1, 2, 3, 4, 5, 6, 7, 8]);
+
+	// return (
+	// 	<div className="p-4">
+	// 		{value.map((val, index) => <p>{val}</p>)}
+	// 		<button className="px-6 py-2 bg-blue-500 rounded-md" onClick={() => setValue([...value, 9])}>click</button>
+	// 	</div>
+	// );
+
+	// 10. print multiple obj value 
+	const [value, setValue] = useState([
+		{name: 'kish', age: 12},
+		{name: 'vish', age: 22},
+		{name: 'kioh', age: 45},
+	]);
+
+	return (
+		<div className="p-4">
+			{value.map( (item) => <p>name: {item.name} age:{item.age}</p> )}
+			<button onClick={() => setValue(() => value.map(item => item.name === 'vish' ? ({name: 'vish', age: 2999}) : item ))}>click</button>
+		</div>
+	);
 }
 
 export default App;
+ 
