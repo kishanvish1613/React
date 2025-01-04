f492d75614bb0e93d49f747aa6a19d80935b9bc6
kishanvish1613 <kishanvishwakarma1613@gmail.com>
2025-01-04 13:50:13 +0530

    form handling in multiple ways

---
+++ 20.13/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+// form handling in react, there are multiple ways to do
+// 1. useRef
+// 2. controlled components
+// 3. react hook form
+
+function App() {
+    return (
+        <div>
+            <p>k</p>
+        </div>
+    )
+}
+
+
+export default App;
