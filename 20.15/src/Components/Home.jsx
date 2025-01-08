6d7a4e5b4b4943b5ca5b668b1a317dd196d65699
kishanvish1613 <kishanvishwakarma1613@gmail.com>
2025-01-08 17:49:28 +0530

    add css properties using multi ways

--- 20.15/src/Components/Home.jsx
+++ 20.15/src/Components/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Home() {
     return (
-        <div>
+        <div className="">
             <p>Home</p>
         </div>
     )
--- 20.15/src/Components/Nav.jsx
+++ 20.15/src/Components/Nav.jsx
@@ -1,13 +1,23 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import {NavLink} from "react-router-dom"
 
 function Nav() {
     return (
-        <div className="">
-			<Link to="/">Home</Link>
-			<Link to="/about">About</Link>
-			<Link to="/contact">Contact</Link>
+        <div className="w-full h-max p-4 bg-zinc-300 flex justify-center items-center gap-10">
+			<NavLink style={(e) => { return {color: e.isActive ? "orange" : "", fontWeight: e.isActive ? "bold" : ""}}} to="/">Home</NavLink>
+			<NavLink style={(e) => { return {color: e.isActive ? "orange" : "", fontWeight: e.isActive ? "bold" : ""}}} to="/about">About</NavLink>
+			<NavLink style={(e) => { return {color: e.isActive ? "orange" : "", fontWeight: e.isActive ? "bold" : ""}}} to="/contact">Contact</NavLink>
 		</div>
+
+
+        // Add multiple css properties
+
+        // < className={(e) => {
+        //     return [
+        //         e.isActive ? "text-red-200" : "",
+        //         e.isActive ? "font-bold" : "",
+        //     ].join(" ");
+        // }}>About</>
     )
 }
 
